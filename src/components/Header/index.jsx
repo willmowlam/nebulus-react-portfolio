d9db6d299b5f234c42755543e391648f957ad78c
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-18 22:30:21 +0000

    Test commit after setup of branch restriction rules

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,6 +10,7 @@ function Header() {
           <Navbar.Brand href="#home">Will Mowlam</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
