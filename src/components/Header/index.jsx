fe4f859bee798bcb4c54539f5f0da748e51e8881
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-19 16:49:54 +0000

    Add Navigation Routing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
       <Navbar sticky="top" bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Will Mowlam</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Will Mowlam</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
     </>
   );
-}
+};
 
 export default Header;
