e531735e955da21cb33f357714e0cabe306f25f1
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-19 19:32:09 +0000

    Fix broken contact link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -7,7 +8,7 @@ function Footer() {
         <Row>  
           <Col><a href="https://www.linkedin.com/in/will-mowlam">LinkedIn</a></Col>
           <Col><a href="https://github.com/willmowlam/">GitHub</a></Col> 
-          <Col><a href="/contact">Contact</a></Col>
+          <Col><Link to="/contact">Contact</Link></Col>
         </Row>
         <Row>
           <p>Copyright &copy; Will Mowlam</p>
