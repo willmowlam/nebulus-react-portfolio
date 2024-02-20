e6d0bc34d5e5e975ef65b7728276408fee797f02
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-20 16:46:16 +0000

    Add handleSubmit to ContactForm

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -2,13 +2,29 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function ContactForm() {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    
+    fetch('/', {
+      method: 'POST',
+      body: new FormData(form),
+    })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+  };
+
   return (
-    <Form name="contact" method="POST" data-netlify="true">
+    <Form 
+      name="contact"
+      action="/contact-success"
+      method="POST" 
+      data-netlify="true"
+      onSubmit={handleSubmit}
+    >
       <h3>Send me a message</h3>
-
-      <Form.Control type="hidden" name="subject" value="Message sent from portfolio" />
-
-      <Form.Control type="hidden" name="form-name" value="contact" />
 
       <Form.Group className="mb-3" controlId="formName">
         <Form.Label>Your name:</Form.Label>
