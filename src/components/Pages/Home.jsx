a220becc865b0427b13b041f9a0fb24e99bef205
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-20 22:40:27 +0000

    Simplify copy

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,16 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-// This should be a welcome landing page that contains:
-
-// Your Name
-
-// A headshot of you (or an avatar)
-
-// Your brand statement
-
-// Some indication that this is your portfolio site
-
 function Home() {
   return (
     <Container as="main" id="page-home">
@@ -20,11 +10,8 @@ function Home() {
         </Col>
         <Col className="col-sm-9">
           <h1 className="display-5 fw-bold">Will Mowlam</h1>
-          <p className="col-md-8 fs-4">I am a web developer based in Worthing in southern England. I enjoy building websites with <a href="https://react.dev/">React</a>, <a href="https://nodejs.org/">Node.js</a>, <a href="https://www.drupal.org/">Drupal</a> and <a href="https://laravel.com/">Laravel</a> using frameworks like <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
-          <p>My tools are  <a href="https://lando.dev/">Lando</a>, <a href="https://code.visualstudio.com/">VScode</a>, <a href="https://getcomposer.org/">Composer</a> and <a href="https://git-scm.com/" >Git</a>.</p>
-          <p>I have experience with APIs and am interested in headless, serverless web hosting.</p>
-          <Link to="/contact" className="btn btn-primary btn-md" type="button">Contact Me</Link>
-          <Link to="/projects" className="btn btn-secondary btn-md" type="button">View Portfolio</Link>
+          <p className="col-md-8 fs-4">I am a web developer based in Worthing, England. I enjoy building websites with React, Node.js, Drupal and Laravel using frameworks like Bootstrap.</p>
+          <Link to="/projects" className="btn btn-primary btn-md" type="button">View Portfolio</Link>
         </Col>
       </Row>
     </Container>
