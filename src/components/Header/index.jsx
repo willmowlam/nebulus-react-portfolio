5e7a1c1cdbe6a9f4a48c4d9eeb89d29a2dcc282d
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-20 23:07:56 +0000

    Move nav links to right

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,7 +11,7 @@ function Header() {
           <Navbar.Brand as={Link} to="/"><img src="./assets/images/icons/favicon-32x32.png" alt="WM" width="32" height="32" className="d-inline-block align-text-top" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="main-nav" />
           <Navbar.Collapse id="main-nav">
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
               <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
