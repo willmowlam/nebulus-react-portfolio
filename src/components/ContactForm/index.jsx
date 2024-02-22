8600652cb1b6b2a010c43272cd39fdecd7b30baa
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-22 01:35:44 +0000

    Add modal for success/error message

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,8 +1,21 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 function ContactForm() {
+
+  // Create state variables for modal visibility
+  const [showModal, setShowModal] = useState(false);
+
+  // Create state for the modal message
+  const [modalMessage, setModalMessage] = useState('');
+
+  // Create state for the modal title
+  const [modalTitle, setModalTitle] = useState('');
+  
+  // Close the modal method
+  const handleCloseModal = () => setShowModal(false);
 
   // Create state variables for the form data
   const [formData, setFormData] = useState({
@@ -39,8 +52,16 @@ function ContactForm() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...formData })
     })
-    .then(() => alert("Success!"))
-    .catch(error => alert(error));
+    .then(() => {
+      setModalTitle('Success!');
+      setModalMessage("Thank you for your message!");
+      setShowModal(true);
+    })
+    .catch(error => {
+      etModalTitle('Error');
+      setModalMessage(error);
+      setShowModal(true);
+    });
 
     // Reset form fields
     setFormData({
@@ -51,29 +72,37 @@ function ContactForm() {
   };
 
   return (
-    <form 
-      name="contact"
-      onSubmit={handleFormSubmit}
-      className="mt-3"
-    >
-      <h3>Send me a message</h3>
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Your name:</Form.Label>
-        <Form.Control type="text" name="name" onChange={handleFormFieldChange} value={formData.name} required />
-      </Form.Group>
+    <>
+      <form name="contact" onSubmit={handleFormSubmit} className="mt-3">
+        <h3>Send me a message</h3>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Your name:</Form.Label>
+          <Form.Control type="text" name="name" onChange={handleFormFieldChange} value={formData.name} required />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control type="email" name="email" onChange={handleFormFieldChange} value={formData.email} required />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="email" name="email" onChange={handleFormFieldChange} value={formData.email} required />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="message">
-        <Form.Label>Message:</Form.Label>
-        <Form.Control as="textarea" rows={5} name="message" onChange={handleFormFieldChange} value={formData.message} required />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="message">
+          <Form.Label>Message:</Form.Label>
+          <Form.Control as="textarea" rows={5} name="message" onChange={handleFormFieldChange} value={formData.message} required />
+        </Form.Group>
 
-      <Button variant="primary" type="submit">Submit</Button>
-    </form>
+        <Button variant="primary" type="submit">Submit</Button>
+      </form>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalTitle}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modalMessage}</Modal.Body>
+        <Modal.Footer>
+          <button onClick={handleCloseModal}>Close</button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
