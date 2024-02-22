9e62d66e2c447083b38a2843743f91d8839f62f7
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-22 00:33:23 +0000

    Add setState and event handlers

--- src/components/Pages/ContactTest.jsx
+++ src/components/Pages/ContactTest.jsx
@@ -1,5 +1,11 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
+
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
 function ContactTest() {
 
@@ -7,17 +13,57 @@ function ContactTest() {
     document.title = `Contact Test ~ Will Mowlam // Web Developer`;    
   }, []);
 
+  // Create state variables for the form data
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''    
+  });
+
+  // Event handler for when form fields change
+  const handleFormFieldChange = (e) => {
+
+    // Create variables from target
+    const {name, value} = e.target;
+
+    // Change the value in the form data state variables
+    setFormData({...formData, [name]: value});
+  };
+
+  // Event handler for form submission
+  const handleFormSubmit = (e) => {
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "test", ...formData })
+    })
+    .then(() => alert("Success!"))
+    .catch(error => alert(error));
+
+    // Stop sending data as querystring 
+    e.preventDefault();
+
+    // Reset form fields
+    setFormData({
+      name: '',
+      email: '',
+      message: ''
+    });
+  };
+
+
   return (
     <Container as="main" id="page-contact">
-      <form name="test" netlify>
+      <form name="test" onSubmit={handleFormSubmit}>
         <p>
-          <label>Name <input type="text" name="name" /></label>
+          <label>Name <input onChange={handleFormFieldChange} type="text" name="name" value={formData.name} required /></label>
         </p>
         <p>
-          <label>Email <input type="email" name="email" /></label>
+          <label>Email <input onChange={handleFormFieldChange} type="email" name="email" value={formData.email} required /></label>
         </p>
         <p>
-          <textarea name="message"></textarea>
+          <textarea onChange={handleFormFieldChange} name="message" value={formData.message} required></textarea>
         </p>
         <p>
           <button type="submit">Send</button>
