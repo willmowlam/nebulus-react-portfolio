d5445021aa4e583334e577b9ab7ac609fd8092de
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-05-17 23:03:41 +0100

    Use html entities for apostrophes

--- src/components/Pages/ContactSuccess.jsx
+++ src/components/Pages/ContactSuccess.jsx
@@ -9,10 +9,10 @@ function ContactSuccess() {
   return (
     <Container as="main" id="page-contact-success">
       <h2>Thank you for your message</h2>
-      <p>I'll be in touch shortly.</p>
+      <p>I&apos;ll be in touch shortly.</p>
       <p>In the mean time, feel free to take a look at some of my recent <a href="/projects">projects</a>.</p>
     </Container>
-    );
-  }
-  
-  export default ContactSuccess;
+  );
+}
+
+export default ContactSuccess;
