f2290a4213f5873fe8ef6f13106f3c656daaa75e
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-05-17 23:01:30 +0100

    Use html entities for apostrophes

--- src/components/Pages/404.jsx
+++ src/components/Pages/404.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 import { setDocumentTitle, setMetaDescription } from '/src/utils';
 
 function Custom404() {
-  
-  setDocumentTitle('404 ~ Will Mowlam // Web Developer');    
+
+  setDocumentTitle('404 ~ Will Mowlam // Web Developer');
   setMetaDescription(`The page or resource doesn't exist.`);
 
   return (
     <Container as="main" id="page-404">
       <h2>404 - Page Not Found</h2>
-      <p>Oops! Looks like you've ventured into uncharted digital territory.</p>
-      <p>No worries! Take a deep breath, and let's navigate back to familiar ground by visiting the home page.</p>
+      <p>Oops! Looks like you&apos;ve ventured into uncharted digital territory.</p>
+      <p>No worries! Take a deep breath, and let&apos;s navigate back to familiar ground by visiting the home page.</p>
       <Link to="/" className="btn btn-primary btn-md" type="button">Home</Link>
     </Container>
-    );
-  }
-  
-  export default Custom404;
+  );
+}
+
+export default Custom404;
